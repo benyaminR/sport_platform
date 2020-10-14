c54b9f6aefdfa5cc9a90aa3685ff2a2cb8075f95
Benyamin <b.radmard77@gmai.com>
2020-10-14 20:22:48 +0200

    + Multidex fixed

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,20 @@ void main() {
   runApp(MyApp());
 }
 
-  class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: MyHomePage(),
-      );
-    }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
   }
+}
+
 
 class MyHomePage extends StatelessWidget {
   @override
