242f3c629d1674a7b5956b32ba62cc145c050056
Christoph Manhart <christoph-manhart@outlook.de>
2021-03-14 15:29:15 +0100

    SH-28 | Discovery-Seite GUI fertigstellen und responsive gemacht

--- lib/discovery.dart
+++ lib/discovery.dart
@@ -9,6 +9,17 @@ import 'discovery_trends.dart';
 class Discovery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Full screen width and height
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+    // Height (without SafeArea)
+    final padding = MediaQuery.of(context).padding;
+    final double height1 = height - padding.top - padding.bottom;
+    // Height (without status bar)
+    final double height2 = height - padding.top;
+    // Height (without status and toolbar)
+    final double height3 = height - padding.top - kToolbarHeight;
+
     return SafeArea(
       child: Container(
         color: Colors.black,
@@ -16,32 +27,36 @@ class Discovery extends StatelessWidget {
           scrollDirection: Axis.vertical,
           children: <Widget>[
             SizedBox(
-              height: 10.0,
+              height: height3 * 0.015,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Icon(Icons.search, size: 34.0, color: Color(0xFF707070),),
+                Icon(
+                  Icons.search,
+                  size: height3 * 0.051,
+                  color: Color(0xFF707070),
+                ),
               ],
             ),
             SizedBox(
-              height: 10.0,
+              height: height3 * 0.015,
             ),
             DiscoveryTrends(),
             SizedBox(
-              height: 10.0,
+              height: height3 * 0.015,
             ),
             DiscoveryTrainers2(),
             SizedBox(
-              height: 10.0,
+              height: height3 * 0.015,
             ),
             DiscoveryCourses(),
             SizedBox(
-              height: 10.0,
+              height: height3 * 0.015,
             ),
             PersonalCourseBox(),
             SizedBox(
-              height: 10.0,
+              height: height3 * 0.015,
             ),
             // DiscoveryContinue(),
           ],
