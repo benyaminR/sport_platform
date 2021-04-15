9a6e24d960e40c313d3226ffb83c381e76001071
Christoph Manhart <christoph-manhart@outlook.de>
2021-04-15 22:26:13 +0200

    SH-63: Formatierung der PersonalCourseBox angepasst

--- lib/personal_course_box_black.dart
+++ lib/personal_course_box_black.dart
@@ -6,12 +6,11 @@ import 'package:sport_platform/course-detail/course_detail_view.dart';
 import 'package:sport_platform/features/storage/presentation/storage/storage_bloc.dart';
 import 'features/courses/domain/enitity/course.dart';
 
-
 class PersonalCourseBoxBlack extends StatelessWidget {
-
   final Course course;
 
-  const PersonalCourseBoxBlack({Key key,@required this.course}) : super(key: key);
+  const PersonalCourseBoxBlack({Key key, @required this.course})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,31 +21,37 @@ class PersonalCourseBoxBlack extends StatelessWidget {
     final double height3 = height - padding.top - kToolbarHeight;
 
     return Padding(
-      padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0, top: 16.0),
+      padding:
+          const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0, top: 16.0),
       child: GestureDetector(
-        onTap: ()=> Navigator.of(context).pushNamed('/home/personal/courseDetailView', arguments : CourseDetailViewData(courseID: course.path)),
+        onTap: () => Navigator.of(context).pushNamed(
+            '/home/personal/courseDetailView',
+            arguments: CourseDetailViewData(courseID: course.path)),
         child: Column(
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.only(left: 0.0, right: 0.0),
               child: Container(
+                width: MediaQuery.of(context).size.width,
+                // height:
+                // (MediaQuery.of(context).size.height) - height3 * 0.52,
+                decoration: new BoxDecoration(
+                  color: Color(0xFF000000),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                      bottomLeft: Radius.circular(20.0),
+                      bottomRight: Radius.circular(20.0)),
+                ),
                 child: Stack(
                   children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: (MediaQuery.of(context).size.height) - height3 * 0.65,
-                      decoration: new BoxDecoration(
-                        color: Color(0xFF000000),
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20.0),
-                            topRight: Radius.circular(20.0),
-                            bottomLeft: Radius.circular(20.0),
-                            bottomRight: Radius.circular(20.0)),
-                      ),
-                    ),
                     Padding(
                       // padding: EdgeInsets.only(top: 230, left: 16.0, right: 16.0),
-                      padding: EdgeInsets.only(top: height3 * 0.34, left: height3 * 0.024, right: height3 * 0.024),
+                      padding: EdgeInsets.only(
+                          top: height3 * 0.34,
+                          bottom: height3 * 0.024,
+                          left: height3 * 0.024,
+                          right: height3 * 0.024),
                       child: Text(
                         course.description,
                         style: TextStyle(
@@ -57,11 +62,14 @@ class PersonalCourseBoxBlack extends StatelessWidget {
                     ),
                     Padding(
                       // padding: EdgeInsets.only(top: 190.0, left: 16.0, right: 16.0),
-                      padding: EdgeInsets.only(top: height3 * 0.28, left: height3 * 0.024, right: height3 * 0.024),
+                      padding: EdgeInsets.only(
+                          top: height3 * 0.28,
+                          left: height3 * 0.024,
+                          right: height3 * 0.024),
                       child: Container(
                         height: height3 * 0.048,
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
                             Text(
                               course.title,
@@ -73,40 +81,55 @@ class PersonalCourseBoxBlack extends StatelessWidget {
                             SizedBox(
                               width: height3 * 0.094,
                             ),
-                            Icon(Icons.person,
-                                size: height3 * 0.020, color: Color(0xFF707070)),
-                            SizedBox(
-                              width: height3 * 0.008,
-                            ),
-                            Text(
-                              course.students.length.toString(),
-                              style: TextStyle(
-                                  color: Color(0xFF707070),
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: height3 * 0.018),
+                            Row(
+                              children: [
+                                Icon(Icons.person,
+                                    size: height3 * 0.020,
+                                    color: Color(0xFF707070)),
+                                SizedBox(
+                                  width: height3 * 0.008,
+                                ),
+                                Text(
+                                  course.students.length.toString(),
+                                  style: TextStyle(
+                                      color: Color(0xFF707070),
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: height3 * 0.018),
+                                ),
+                              ],
                             ),
                             SizedBox(
                               width: height3 * 0.044,
                             ),
-                            Icon(Icons.star, size: height3 * 0.020, color: Colors.white),
-                            Icon(Icons.star, size: height3 * 0.020, color: Colors.white),
-                            Icon(Icons.star, size: height3 * 0.020, color: Colors.white),
-                            Icon(Icons.star, size: height3 * 0.020, color: Colors.white),
+                            Row(
+                              children: [
+                                Icon(Icons.star,
+                                    size: height3 * 0.020, color: Colors.white),
+                                Icon(Icons.star,
+                                    size: height3 * 0.020, color: Colors.white),
+                                Icon(Icons.star,
+                                    size: height3 * 0.020, color: Colors.white),
+                                Icon(Icons.star,
+                                    size: height3 * 0.020, color: Colors.white),
+                              ],
+                            ),
                           ],
                         ),
                       ),
                     ),
-                    BlocProvider.value(value: getIt<StorageBloc>()..add(GetDownloadUrlEvent(path: course.thumbnail)),
-                      child: BlocBuilder<StorageBloc,StorageState>(
+                    BlocProvider.value(
+                      value: getIt<StorageBloc>()
+                        ..add(GetDownloadUrlEvent(path: course.thumbnail)),
+                      child: BlocBuilder<StorageBloc, StorageState>(
                         builder: (context, state) {
-                          if(state is GetDownloadUrlCompleted)
+                          if (state is GetDownloadUrlCompleted)
                             return Container(
                               width: MediaQuery.of(context).size.width,
-                              height: (MediaQuery.of(context).size.height) - height3 * 0.82,
+                              height: (MediaQuery.of(context).size.height) -
+                                  height3 * 0.82,
                               decoration: new BoxDecoration(
                                 image: DecorationImage(
-                                  image: new NetworkImage(
-                                      state.downloadUrl),
+                                  image: new NetworkImage(state.downloadUrl),
                                   fit: BoxFit.cover,
                                 ),
                                 borderRadius: BorderRadius.only(
@@ -119,7 +142,8 @@ class PersonalCourseBoxBlack extends StatelessWidget {
                           return CircularProgressIndicator();
                         },
                       ),
-                    )],
+                    )
+                  ],
                 ),
               ),
             ),
